67f6ab622a1b64cd16df625625aabbbcf62642a2
Thiago Lugli <thiago.lugli@ifood.com.br>
2021-10-20 18:56:51 -0300

    Use atomFamily

--- src/state.ts
+++ src/state.ts
@@ -1,8 +1,9 @@
-import { atom, useAtom } from 'jotai';
+import { Atom, atom, useAtom, WritableAtom } from 'jotai';
 import { useCallback, useMemo } from 'react';
 import { generateOrderProps, getOrderIdbKey, getOrderIdsIdbKey, Order, OrderProps } from './models/Order';
 import { get, set } from 'idb-keyval';
-import { atomWithStorage, useUpdateAtom } from 'jotai/utils';
+import { atomWithStorage, useUpdateAtom, atomFamily } from 'jotai/utils';
+import deepEqual from 'fast-deep-equal'
 
 function atomWithAsyncStorage<T>(key: string, initial: T, storeName?: string) {
   return atomWithStorage<T>(key, initial, {
@@ -18,26 +19,46 @@ function atomWithAsyncStorage<T>(key: string, initial: T, storeName?: string) {
   });
 }
 
-export const ordersAtom = atomWithAsyncStorage<OrderProps[]>('order', []);
+export const ordersAtom = atomWithAsyncStorage<Record<OrderProps['id'], OrderProps>>('order', {});
 
 // export const orderIdsAtom = atomWithAsyncStorage<OrderProps['id'][]>(getOrderIdsIdbKey(), []);
-export const orderIdsAtom = atom((get) => get(ordersAtom).map((order) => order.id));
+export const orderIdsAtom = atom(
+  (get) => Object.keys(get(ordersAtom)));
 
-export const singleOrderAtom = (id: OrderProps['id']) => atom((get) => get(ordersAtom).find((order) => order.id === id)!);
+export const singleOrderAtom = (id: OrderProps['id']) => atom(
+  (get) => new Order(get(ordersAtom)[id]!),
+  (get, set, updatedOrderA: Order) => {
+    const updatedOrder = updatedOrderA.primitify();
+    const orders = get(ordersAtom);
+    orders[updatedOrder.id] = updatedOrder;
+    set(ordersAtom, {...orders});
+    // you can set as many atoms as you want at the same time
+  }
+);
 
-export function useOrder(id: string): [Order, (updatedOrder: any) => void | Promise<void>] {
-  const orderAtom = singleOrderAtom(id);
-  const [order, updateOrder] = useAtom(orderAtom);
-  const updateOrders = useUpdateAtom(ordersAtom);
+// atomFamily<Param, AtomType extends Atom<unknown>>(initializeAtom: (param: Param) => AtomType, areEqual?: (a: Param, b: Param) => boolean): AtomFamily<Param, AtomType>;
 
-  const orderInstance = useMemo(() => new Order(order), [order]);
+// declare type AtomFamily<Param, AtomType> = {
+//   (param: Param): AtomType;
+//   remove(param: Param): void;
+//   setShouldRemove(shouldRemove: ShouldRemove<Param> | null): void;
+// };
 
-  const updateOrderFromIntance = useCallback((updatedOrder) => {
-    const primitifiedOrder = updatedOrder.primitify();
-    updateOrders((currentOrders) => currentOrders.map((order) => order.id === primitifiedOrder.id ? primitifiedOrder : order));
-  }, [updateOrder]);
+const orderAtom = atomFamily<string, WritableAtom<Order, Order, void>>((id) => singleOrderAtom(id), deepEqual);
 
-  return [orderInstance, updateOrderFromIntance];
+export function useOrder(id: string) {
+  const myAtom = useMemo(() => orderAtom(id), [id]);
+  const atomReturn = useAtom(myAtom);
+  return atomReturn;
+
+  // const orderInstance = useMemo(() => new Order(order), [order]);
+
+  // const updateOrderFromIntance = useCallback((updatedOrder) => {
+  //   const primitifiedOrder = updatedOrder.primitify();
+  //   updateOrders();
+  // }, [updateOrders]);
+
+  // return [orderInstance, updateOrderFromIntance];
 }
 
 export function useOrdersIds() {
@@ -55,7 +76,7 @@ export function useCreateOrder() {
     //   getOrderIdbKey(newOrderProps.id),
     //   newOrderProps
     // );
-    setOrders((currentOrders) => [...currentOrders, newOrderProps]);
+    setOrders((currentOrders) => ({...currentOrders, [newOrderProps.id]: newOrderProps}));
   }, [setOrders]);
 
   return addOrder;
